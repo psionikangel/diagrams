8986ebf08facc96b3058572593dee6c8b89cb628
Jérémie Carpentier-Roy <jeremie.carpentier-roy@montreal.ca>
2023-11-23 20:59:42 +0000

    update diagram paas
    Signed-off-by: Jérémie Carpentier-Roy <jeremie.carpentier-roy@montreal.ca>

--- my-diagrams/my_diagrams/paas.py
+++ my-diagrams/my_diagrams/paas.py
@@ -1,13 +1,13 @@
-from diagrams import Cluster, Diagram
+from diagrams import Cluster, Diagram, Edge
 from diagrams.k8s.infra import Node
 from diagrams.onprem.ci import Jenkins
 from diagrams.onprem.vcs import Git
 
 with Diagram("PaaS/Kubernetes", show=False, filename="images/paas"):
     jenkins = Jenkins("Jenkins")
-    bitbucket = Git("Bitbucket")
+    bitbucket = Git("Bitbucket \n [Code applicatif]")
 
     with Cluster("Environnement lab/dev/acc/prd"):
-        noeud = Node("Worker")
+        noeud = Node("Worker/Noeud")
     
-    bitbucket << jenkins >> noeud
+    bitbucket << Edge(label="sync") << jenkins >> Edge(label="deploie") >> noeud
