eca3c68f7319a90edac31bc6066b7b1e094ae870
Jérémie Carpentier-Roy <jeremie.carpentier-roy@montreal.ca>
2023-11-23 20:44:21 +0000

    ajout du diagramme paas
    Signed-off-by: Jérémie Carpentier-Roy <jeremie.carpentier-roy@montreal.ca>

--- my-diagrams/my_diagrams/paas.py
+++ my-diagrams/my_diagrams/paas.py
@@ -1,6 +1,13 @@
 from diagrams import Cluster, Diagram
 from diagrams.k8s.infra import Node
+from diagrams.onprem.ci import Jenkins
+from diagrams.onprem.vcs import Git
 
 with Diagram("PaaS/Kubernetes", show=False, filename="images/paas"):
+    jenkins = Jenkins("Jenkins")
+    bitbucket = Git("Bitbucket")
+
     with Cluster("Environnement lab/dev/acc/prd"):
-        Node("Worker")
+        noeud = Node("Worker")
+    
+    bitbucket << jenkins >> noeud
