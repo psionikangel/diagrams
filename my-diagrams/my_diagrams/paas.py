c92384333ef210f1dea8a2c22f35fa71fd71bfa5
Jérémie Carpentier-Roy <jeremie.carpentier-roy@montreal.ca>
2023-11-24 15:15:55 +0000

    precision du diagramme paas
    Signed-off-by: Jérémie Carpentier-Roy <jeremie.carpentier-roy@montreal.ca>

--- my-diagrams/my_diagrams/paas.py
+++ my-diagrams/my_diagrams/paas.py
@@ -1,5 +1,5 @@
 from diagrams import Cluster, Diagram, Edge
-from diagrams.k8s.infra import Node
+from diagrams.k8s.compute import Pod
 from diagrams.onprem.ci import Jenkins
 from diagrams.onprem.vcs import Git
 
@@ -7,7 +7,7 @@ with Diagram("PaaS/Kubernetes", show=False, filename="images/paas"):
     jenkins = Jenkins("Jenkins")
     bitbucket = Git("Bitbucket \n [Code applicatif]")
 
-    with Cluster("Environnement lab/dev/acc/prd"):
-        noeud = Node("Worker/Noeud")
+    with Cluster("Worker/Noeud (lab/dev/acc/prd)"):
+        noeud = [Pod("web"),Pod("api")]
     
     bitbucket << Edge(label="sync") << jenkins >> Edge(label="deploie") >> noeud
